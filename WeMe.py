168045f62ed4886d0225cb00e6a8239828d16a6c
Mohammad Abdullah <162617221+Abdullah5590@users.noreply.github.com>
2025-08-13 04:56:28 +0530

    Update WeMe.py

--- WeMe.py
+++ WeMe.py
@@ -6,7 +6,7 @@ import os
 # st.set_page_config(page_title="WeMe", page_icon="", layout="wide")
 st.set_page_config(
     page_title="WeMe",
-    page_icon="https://github.com/Abdullah5590/WeMe_Ui/blob/main/1.jpeg",  # Path to your logo file
+    page_icon="https://raw.githubusercontent.com/Abdullah5590/WeMe_Ui/main/1.jpeg",  # Path to your logo file
     layout="wide"
 )
 
@@ -378,4 +378,5 @@ elif menu == "Contact US":
                 st.success("✅ Thank you for reaching out! I will get back to you soon.")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
